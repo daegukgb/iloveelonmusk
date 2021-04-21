7c812dd49113710584379a05b232fe241ace4d84
Gyubeen <32409617+daegukgb@users.noreply.github.com>
2021-04-21 15:02:51 +0900

    Update ConnectServer.cpp

--- ConnectServer.cpp
+++ ConnectServer.cpp
@@ -1,69 +1,59 @@
-#include <bits/stdc++.h>
+#pragma comment(lib, "ws2_32.lib") 
+#include <iostream>
 #include <thread>
 #include <winsock2.h>
-#include "Commands.h"
+#include <string>
 
 using namespace std;
 
 extern WSADATA wsaData;
-extern SOCKET clientSocket;
-extern SOCKADDR_IN serverAddress;
+extern SOCKET hSocket;
+extern SOCKADDR_IN servAddr;
+
+void ErrorHandling(char* message);
+
 
 void ShowErrorMessage(string message)
 {
-	cout << "[오류발생]: " << message << '\n';
+	cout << "[error]: " << message << '\n';
 	system("pause");
 	exit(1);
 }
 
-void receiveData() {
-	char received[256];
-	string sent;
-	while (1) {
-		int length = recv(clientSocket, received, sizeof(received), 0);
-		received[length] = '\0';
-		if (strcmp(received, "[exit]") == 0) {
-			break;
-		}
-		cout << "[서버메시지]: " << received << '\n';
-	}
-}
+//void listener()
 
-void sendData(string sent) {
-	while (1) { 
-		send(clientSocket, sent.c_str(), sent.length(), 0);
-	}
+void getConnecting(string ip, int port) {
+	WSADATA wsaData;
+	SOCKET hSocket;
+	SOCKADDR_IN servAddr;
+
+	char message[30];
+	int strLen;
+
+
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		ShowErrorMessage("WSAStartup() error!");
+
+	hSocket = socket(PF_INET, SOCK_STREAM, 0);
+	if (hSocket == INVALID_SOCKET)
+		ShowErrorMessage("socket() error");
+
+	memset(&servAddr, 0, sizeof(servAddr));
+	servAddr.sin_family = AF_INET;
+	servAddr.sin_addr.s_addr = inet_addr("192.168.2.14");
+	servAddr.sin_port = htons(5012);
+
+	if (connect(hSocket, (SOCKADDR*)&servAddr, sizeof(servAddr)) == SOCKET_ERROR)
+		ShowErrorMessage("connect() error!");
 	
-}
+	strLen = recv(hSocket, message, sizeof(message) - 1, 0);
+	if (strLen == -1)
+		ShowErrorMessage("read() error!");
+	cout << "Message from server: %s "<< '\n'<< message << '\n';
+	const char* test = "test";
+	send(hSocket, test, sizeof(test), 0);
 
-void closeConnect() {
-	closesocket(clientSocket);
+	closesocket(hSocket);
 	WSACleanup();
-}
- 
-void getConnecting() {
-	
-	int serverPort = 9876;
-	char received[256];
-	string sent;
-	
-	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) // Winsock을초기화합니다.
-		ShowErrorMessage("WSAStartup()");
-	clientSocket = socket(PF_INET, SOCK_STREAM, 0); // TCP 소켓을생성합니다.
-	if (clientSocket == INVALID_SOCKET)
-		ShowErrorMessage("socket()");
-	
-	memset(&serverAddress, 0, sizeof(serverAddress));
-	serverAddress.sin_family = AF_INET;
-	serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1"); // 문자열IP를네트워크바이트형식으로
-	serverAddress.sin_port = htons(serverPort); // 2바이트정수네트워크바이트형식으로
-	
-	if (connect(clientSocket, (SOCKADDR*)&serverAddress, sizeof(serverAddress)) == SOCKET_ERROR)
-		ShowErrorMessage("connect()");
-	cout << "연결됨\n"; 
-	thread iThread(&receiveData);
-	thread oThread(&sendData);
-	iThread.join();
-	oThread.join(); 
-	
+
 }
